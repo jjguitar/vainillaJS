8a0d0f6365718d39938176b269e7ac911efb6df2
Jhoncito el pillo <jhon.electronica@gmail.com>
2020-07-20 11:02:19 -0500

    Fix home route

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
   <div class="Header-main">
     <div class="Header-logo">
       <h1>
-        <a href="/">
+        <a href="/vainillaJS/">
           100tifi.co
         </a>
       </h1>
